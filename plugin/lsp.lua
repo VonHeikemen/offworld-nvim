1a8a2ac1dc90c07d703022d81536e890be70f276
Heiker Curiel <heikercuriel@gmail.com>
2024-07-25 11:52:12 -0400

    feat: add intelephense lsp config

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -14,13 +14,20 @@ offworld.ui = {
 }
 
 offworld.lsp_attach = function(_, bufnr)
-  -- keymaps defined in lua/user/keymaps.lua
-  offworld.lsp_keymaps(bufnr)
+	-- keymaps defined in lua/user/keymaps.lua
+	offworld.lsp_keymaps(bufnr)
 
   -- enable diagnostic icon in statusline
-  vim.b.user_diagnostic_status = 1
+	vim.b.user_diagnostic_status = 1
 end
 
+---
+-- I like to start language servers manually
+-- So I use the command :LspSetup {server_name}
+-- Where {server_name} is a property in offworld.lspconfig
+---
+
+-- :LspSetup lua_ls
 offworld.lspconfig.lua_ls = function()
   local dir = require('offworld.dir')
 
@@ -29,6 +36,19 @@ offworld.lspconfig.lua_ls = function()
     filetypes = {'lua'},
     root_dir = function(bufnr)
       return dir.find_first(bufnr, {'.luarc.json'})
+    end,
+  }
+end
+
+-- :LspSetup intelephense
+offworld.lspconfig.intelephense = function()
+  local dir = require('offworld.dir')
+
+  return {
+    cmd = {'intelephense', '--stdio'},
+    filetypes = {'php'},
+    root_dir = function(bufnr)
+      return dir.find_first(bufnr, {'composer.json'})
     end,
   }
 end
