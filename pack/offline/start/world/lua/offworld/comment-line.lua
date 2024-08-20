2c5ba34b838d5b3428a80615e066bbeceed7012f
kuro337 <cb.kalz@gmail.com>
2024-08-20 04:35:23 -0400

    fixed range logic to work from bottom to up as well

--- pack/offline/start/world/lua/offworld/comment-line.lua
+++ pack/offline/start/world/lua/offworld/comment-line.lua
@@ -3,18 +3,16 @@ local s = {}
 local block_comment = '%s%s%s%s'
 local line_comment = '%s%s%s'
 
-s.with_range = {'v', 'V', '\22', '\22s', 's', 'S'}
-s.not_supported = {'o', 'c', 'cv', 'ce', '!', 't'}
+s.with_range = { 'v', 'V', '\22', '\22s', 's', 'S' }
+s.not_supported = { 'o', 'c', 'cv', 'ce', '!', 't' }
 
-if _G._user_comment_line == nil then
-  _G._user_comment_line = function() M.toggle() end
-end
+if _G._user_comment_line == nil then _G._user_comment_line = function() M.toggle() end end
 
 -- Make toggle() dot-repeatable
 function M.toggle_op()
   -- for more details, see:
   -- https://gist.github.com/kylechui/a5c1258cd2d86755f97b10fc921315c3
-  vim.o.operatorfunc = "v:lua._user_comment_line"
+  vim.o.operatorfunc = 'v:lua._user_comment_line'
   return 'g@l'
 end
 
@@ -32,17 +30,22 @@ function M.toggle()
   if vim.tbl_contains(s.with_range, mode) then
     local b = vim.fn.getpos('v')
     local e = vim.fn.getpos('.')
-    range = {start = b[2], ends = e[2]}
+    if b[2] > e[2] then
+      local temp = b
+      b = e
+      e = temp
+    end
+    range = { start = b[2], ends = e[2] }
   elseif vim.tbl_contains(s.not_supported, mode) then
     vim.notify('[comment-line] Current mode is not supported', vim.log.levels.WARN)
     return
   else
     local lnum = vim.fn.line('.')
-    range = {start = lnum, ends = lnum}
+    range = { start = lnum, ends = lnum }
   end
 
   local l, r = cs:match('^%s*(.*)%%s(.-)%s*$')
-  local input = {left = l, right = vim.trim(r) == '' and '' or r}
+  local input = { left = l, right = vim.trim(r) == '' and '' or r }
 
   local new = {}
   local uncomment
@@ -53,14 +56,12 @@ function M.toggle()
   for _, line in ipairs(lines) do
     if #vim.trim(line) >= 1 then
       uncomment = s.is_comment(input, line)
-      first_indent = {line:find('^(%s*)')}
+      first_indent = { line:find('^(%s*)') }
       break
     end
   end
 
-  if uncomment == nil then
-    return
-  end
+  if uncomment == nil then return end
 
   input.default_indent = first_indent
   local transform = uncomment and s.uncomment_line or s.comment_line
@@ -80,9 +81,7 @@ function s.is_comment(opts, text)
 end
 
 function s.comment_line(opts, text)
-  if text == '' then
-    return text
-  end
+  if text == '' then return text end
 
   local di = opts.default_indent
   local _, indent, pad = text:find('^(%s*)')
@@ -106,9 +105,7 @@ function s.comment_line(opts, text)
 end
 
 function s.uncomment_line(opts, text)
-  if text == '' then
-    return text
-  end
+  if text == '' then return text end
 
   local start = text:find(opts.left:sub(1, 1))
   local index = start + #opts.left
@@ -117,16 +114,13 @@ function s.uncomment_line(opts, text)
     ends = #text
   else
     ends = #text - #opts.right
-  end 
+  end
 
   local code = text:sub(index, ends)
-  if start == 1 then
-    return code
-  end
+  if start == 1 then return code end
 
   local indent = text:sub(1, start - 1)
   return string.format('%s%s', indent, code)
 end
 
 return M
-
